3232cfd57c2cc1c72a14008a4c505b01255bf138
MarleneMS09 <marlenemsanchez05@gmail.com>
2023-05-16 12:04:11 -0600

    finalizacion de proyecto, error linea 18 doc post

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import ListPost from "../components/ListPosts"
+import ListCategories from "../components/ListCategories"
+
+const Home = () => {
+  return (
+    <main>
+      <div className="container">
+        <h2 className="title-page">Pet noticias</h2>
+      </div>
+      <ListCategories />
+      <ListPost url={"/posts"}/>
+    </main>
+  )
+}
+
+export default Home
